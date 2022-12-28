7bd16c250183261666889990a3769ed58222b4da
22a91a05d1 <119841034+22a91a05d1@users.noreply.github.com>
2022-12-28 11:10:39 +0530

    #include<stdio.h> int main() {     int n,x,i;     scanf("%d%d",&n,&x);     for(i=n;i<=x;i++)     {         printf("%d ",i);     } }

--- A_to_B.c
+++ A_to_B.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int i,a,b;
-    scanf("%d%d",&a,&b);
-    for(i=a;i<=b;i++)
+    int n,x,i;
+    scanf("%d%d",&n,&x);
+    for(i=n;i<=x;i++)
     {
         printf("%d ",i);
     }
